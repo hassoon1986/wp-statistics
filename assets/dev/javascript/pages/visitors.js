8e633deae56fcf89cba1e2fa31304e69c62767f1
Mehrshad Darzi <mehrshad198@gmail.com>
2019-07-02 17:29:21 +0430

    Minor Improvement

--- assets/dev/javascript/pages/visitors.js
+++ assets/dev/javascript/pages/visitors.js
@@ -38,7 +38,7 @@ if (wps_js.isset(wps_js.global, 'request_params', 'page') && wps_js.global.reque
 
                             // Set LocalStorage , Cached for 3 Hour
                             localStorage.setItem('wp-statistics-visitors-filter', JSON.stringify({
-                                value: JSON.stringify(data),
+                                value: data,
                                 timestamp: (new Date().getTime() + (3 * 60 * 60 * 1000))
                             }));
 
@@ -50,7 +50,7 @@ if (wps_js.isset(wps_js.global, 'request_params', 'page') && wps_js.global.reque
                         }
                     });
                 } else {
-                    wp_statistics_show_visitors_filter(tickBox_DIV, JSON.parse(filter_data['value']));
+                    wp_statistics_show_visitors_filter(tickBox_DIV, filter_data['value']);
                 }
 
             }
