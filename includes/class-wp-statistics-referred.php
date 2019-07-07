241146c93c221ccf6542a7eedb4642e7a68643b7
Mehrshad Darzi <mehrshad198@gmail.com>
2019-07-07 19:24:10 +0430

    improvement refer Sql method

--- includes/class-wp-statistics-referred.php
+++ includes/class-wp-statistics-referred.php
@@ -223,19 +223,10 @@ class Referred {
 	public static function getTop( $number = 10 ) {
 		global $wpdb;
 
-		// Check Protocol Of domain
-		$where       = '';
-		$domain_name = rtrim( preg_replace( '/^https?:\/\//', '', get_site_url() ), " / " );
-		foreach ( array( "http", "https", "ftp" ) as $protocol ) {
-			foreach ( array( '', 'www.' ) as $w3 ) {
-				$where = " AND `referred` NOT LIKE '{$protocol}://{$w3}{$domain_name}%' ";
-			}
-		}
-
 		//Get Top Referring
 		if ( false === ( $get_urls = get_transient( self::$top_referring_transient ) ) ) {
 
-			$result = $wpdb->get_results( "SELECT SUBSTRING_INDEX(REPLACE( REPLACE( referred, 'http://', '') , 'https://' , '') , '/', 1 ) as `domain`, count(referred) as `number` FROM " . DB::table( 'visitor' ) . " WHERE `referred` REGEXP \"^(https?://|www\\.)[\.A-Za-z0-9\-]+\\.[a-zA-Z]{2,4}\" AND referred <> '' AND LENGTH(referred) >=12 {$where} GROUP BY domain ORDER BY `number` DESC LIMIT $number" );
+			$result = $wpdb->get_results( self::GenerateReferSQL( "ORDER BY `number` DESC LIMIT $number", '' ) );
 			foreach ( $result as $items ) {
 				$get_urls[ $items->domain ] = self::get_referer_from_domain( $items->domain );
 			}
@@ -322,14 +313,6 @@ class Referred {
 			$where = "AND `last_counter` BETWEEN '" . $args['from'] . "' AND '" . $args['to'] . "' ";
 		}
 
-		// Check Protocol Of domain
-		$domain_name = rtrim( preg_replace( '/^https?:\/\//', '', get_site_url() ), " / " );
-		foreach ( array( "http", "https", "ftp" ) as $protocol ) {
-			foreach ( array( '', 'www.' ) as $w3 ) {
-				$where = " AND `referred` NOT LIKE '{$protocol}://{$w3}{$domain_name}%' ";
-			}
-		}
-
 		// Check Min Number
 		$having = '';
 		if ( isset( $args['min'] ) ) {
@@ -343,6 +326,27 @@ class Referred {
 		}
 
 		// Return List
-		return $wpdb->get_results( "SELECT SUBSTRING_INDEX(REPLACE( REPLACE( referred, 'http://', '') , 'https://' , '') , '/', 1 ) as `domain`, count(referred) as `number` FROM " . DB::table( 'visitor' ) . " WHERE `referred` REGEXP \"^(https?://|www\\.)[\.A-Za-z0-9\-]+\\.[a-zA-Z]{2,4}\" AND referred <> '' AND LENGTH(referred) >=12 " . $where . " GROUP BY domain " . $having . " ORDER BY `number` DESC " . $limit );
+		return $wpdb->get_results( self::GenerateReferSQL( $having . " ORDER BY `number` DESC " . $limit, $where ) );
+	}
+
+	/**
+	 * Generate Basic SQL Refer List
+	 *
+	 * @param string $where
+	 * @param string $extra
+	 * @return string
+	 */
+	public static function GenerateReferSQL( $extra = '', $where = '' ) {
+
+		// Check Protocol Of domain
+		$domain_name = rtrim( preg_replace( '/^https?:\/\//', '', get_site_url() ), " / " );
+		foreach ( array( "http", "https", "ftp" ) as $protocol ) {
+			foreach ( array( '', 'www.' ) as $w3 ) {
+				$where = " AND `referred` NOT LIKE '{$protocol}://{$w3}{$domain_name}%' ";
+			}
+		}
+
+		// Return SQL
+		return "SELECT SUBSTRING_INDEX(REPLACE( REPLACE( referred, 'http://', '') , 'https://' , '') , '/', 1 ) as `domain`, count(referred) as `number` FROM " . DB::table( 'visitor' ) . " WHERE `referred` REGEXP \"^(https?://|www\\.)[\.A-Za-z0-9\-]+\\.[a-zA-Z]{2,4}\" AND referred <> '' AND LENGTH(referred) >=12 " . $where . " GROUP BY domain " . $extra;
 	}
 }
