75ec501723c5977ae3eee2c31696d04d4a32af35
Mehrshad Darzi <mehrshad198@gmail.com>
2019-08-06 13:00:05 +0430

    Add Custom Page Chart Method #272

--- includes/admin/pages/class-wp-statistics-admin-page-pages.php
+++ includes/admin/pages/class-wp-statistics-admin-page-pages.php
@@ -5,12 +5,25 @@ namespace WP_STATISTICS;
 class pages_page {
 
 	public function __construct() {
+		global $wpdb;
 
 		if ( Menus::in_page( 'pages' ) ) {
 
 			// Disable Screen Option
 			add_filter( 'screen_options_show_screen', '__return_false' );
+
+			// Check Exist Statistics For Custom Page
+			if ( self::is_custom_page() ) {
+				$page_count = $wpdb->get_var( "SELECT COUNT(*) FROM " . DB::table( 'pages' ) . " WHERE `id` = " . esc_sql( $_GET['ID'] ) . " AND `type` = '" . esc_sql( $_GET['type'] ) . "'" );
+				if ( $page_count < 1 ) {
+					wp_die( __( 'Your request is not valid.', 'wp-statistics' ) );
+				}
+			}
 		}
+	}
+
+	public static function is_custom_page() {
+		return ( isset( $_GET['ID'] ) and isset( $_GET['type'] ) );
 	}
 
 	/**
@@ -19,6 +32,12 @@ class pages_page {
 	 * @throws \Exception
 	 */
 	public static function view() {
+
+		// Check Show Custom Page
+		if ( self::is_custom_page() ) {
+			self::custom_page_statistics();
+			exit;
+		}
 
 		// Page title
 		$args['title'] = __( 'Top Pages', 'wp-statistics' );
@@ -41,6 +60,32 @@ class pages_page {
 
 		// Show Template Page
 		Admin_Template::get_template( array( 'layout/header', 'layout/title', 'pages/pages', 'layout/postbox.toggle', 'layout/footer' ), $args );
+	}
+
+	/**
+	 * @throws \Exception
+	 */
+	public static function custom_page_statistics() {
+
+		// Page ID
+		$ID   = esc_html( $_GET['ID'] );
+		$Type = esc_html( $_GET['type'] );
+
+		// Page title
+		$args['title'] = __( 'Page Statistics', 'wp-statistics' );
+
+		// Get Current Page Url
+		$args['pageName']   = Menus::get_page_slug( 'pages' );
+		$args['custom_get'] = array(
+			'ID'   => $ID,
+			'type' => $Type
+		);
+
+		// Get Date-Range
+		$args['DateRang'] = Admin_Template::DateRange();
+
+		// Show Template Page
+		Admin_Template::get_template( array( 'layout/header', 'layout/title', 'layout/date.range', 'pages/page-chart', 'layout/footer' ), $args );
 	}
 
 }
