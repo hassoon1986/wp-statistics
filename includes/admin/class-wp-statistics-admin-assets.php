18b678fad5158653d7c4d4ed3ee0eaf2998727c0
Mehrshad Darzi <mehrshad198@gmail.com>
2019-08-06 13:34:12 +0430

    Load Select2 Jquery Library in admin custom chart Pages #268

--- includes/admin/class-wp-statistics-admin-assets.php
+++ includes/admin/class-wp-statistics-admin-assets.php
@@ -116,7 +116,7 @@ class Admin_Assets {
 		}
 
 		// Load Select2
-		if ( Menus::in_page( 'visitors' ) ) {
+		if ( Menus::in_page( 'visitors' ) || ( Menus::in_page( 'pages' ) and isset( $_GET['ID'] ) ) ) {
 			wp_enqueue_style( self::$prefix . '-select2', self::url( 'select2/select2.min.css' ), array(), '4.0.7' );
 		}
 	}
@@ -149,7 +149,7 @@ class Admin_Assets {
 		}
 
 		// Load Select2
-		if ( Menus::in_page( 'visitors' ) ) {
+		if ( Menus::in_page( 'visitors' ) || ( Menus::in_page( 'pages' ) and isset( $_GET['ID'] ) ) ) {
 			wp_enqueue_script( self::$prefix . '-select2', self::url( 'select2/select2.full.min.js' ), array( 'jquery' ), '4.0.7' );
 		}
 
