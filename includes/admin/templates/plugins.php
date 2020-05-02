0e2a21aa36e7ebff5dd4285eddeeb91572994e7c
Mostafa Soufi <mostafa.soufi@hotmail.com>
2020-05-02 21:56:12 +0300

    Fix missed method on plugins.php

--- includes/admin/templates/plugins.php
+++ includes/admin/templates/plugins.php
@@ -1,9 +1,5 @@
 <div class="wrap wps-wrap">
-	<?php use WP_STATISTICS\Admin_Helper;
-	use WP_STATISTICS\Admin_Template;
-
-	Admin_Template::show_page_title( __( 'Extensions for WP-Statistics', 'wp-statistics' ) ); ?>
-
+	<?php _e( 'Extensions for WP-Statistics', 'wp-statistics' ); ?>
     <p><p><?php _e( 'These extensions add functionality to your WP-Statistics.', 'wp-statistics' ); ?></p><br/></p>
     <?php include( WP_STATISTICS_DIR . "includes/admin/templates/add-ons.php" ); ?>
 </div>
